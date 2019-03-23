04c77246da4d615ffd590be9aefdc49e41c652a3
tibo <tibo@inclusivitysolutions.com>
2019-03-24 01:38:37 +0200

    Update folder structure

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navigation = () => {
+  return (<div>
+            <h1>Navigation</h1>
+          </div>)
+};
+
+export default Navigation;
---
+++ src/components/Session/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Session = () => {
+  return (<div>
+            <h1>Session</h1>
+          </div>)
+};
+
+export default Session;
