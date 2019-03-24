58b31bf9c48eb12ae08611d02bb1f1eab1a7c9a7
tibo <tibo@inclusivitysolutions.com>
2019-03-24 18:05:55 +0200

    List users in admin page

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,9 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Admin = () => {
-  return (<div>
-            <h1>Admin</h1>
-          </div>)
-};
+import { withFirebase } from '../Firebase';
 
-export default Admin;
+class AdminPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      users: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    this.props.firebase.users().on('value', snapshot => {
+      const usersObject = snapshot.val();
+
+      const usersList = Object.keys(usersObject).map(key => ({
+          ...usersObject[key],
+          uid: key,
+        }));
+      console.log(usersObject)
+      console.log(usersList)
+      console.log(JSON.stringify(usersList))
+
+      this.setState({
+        users: usersList,
+        loading: false,
+      });
+    });
+  }
+
+  componentWillMount() {
+    this.props.firebase.users().off();
+  }
+
+  render() {
+    const { users, loading } = this.state;
+    console.log(users);
+
+    return (
+      <div>
+        <h1>Admin</h1>
+        { loading && <div>Loading ...</div> }
+
+        <UserList users={users} />
+      </div>
+    );
+  }
+}
+
+const UserList = ({ users }) => (
+  <ul>
+    {users.map(user => (
+      <li key={user.uid}>
+        <span>
+          <strong>ID:</strong> {user.uid}
+        </span>
+        <span>
+          <strong>E-Mail:</strong> {user.email}
+        </span>
+        <span>
+          <strong>Username:</strong> {user.username}
+        </span>
+      </li>
+      ))}
+  </ul>
+);
+
+export default withFirebase(AdminPage);
