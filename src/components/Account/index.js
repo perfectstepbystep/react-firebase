35c9d86dfa4bc7579eb3003c90edc7a98523f5de
tibo <tibo@inclusivitysolutions.com>
2019-03-24 16:52:56 +0200

    Add authorization to home & account pages

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 
 import { PasswordForgetForm } from '../PasswordForget';
 import PasswordChangeForm from '../PasswordChange';
+import { AuthUserContext, withAuthorization } from '../Session';
 
 const AccountPage = () => (
-  <div>
-    <h1>Account Page</h1>
-    <PasswordForgetForm />
-    <PasswordChangeForm />
-  </div>
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <div>
+        <h1>Account: {authUser.email}</h1>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
+      </div>
+    )}
+  </AuthUserContext.Consumer>
 );
+const condition = authUser => authUser !== null;
 
-export default AccountPage;
+export default withAuthorization(condition)(AccountPage);
