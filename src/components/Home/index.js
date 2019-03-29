719dc369bd521b3c01d8afe0a7a7e09e5223c3bf
tibo <tibo@inclusivitysolutions.com>
2019-03-30 00:55:47 +0200

    Can delete a message

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -64,6 +64,10 @@ class MessagesBase extends Component {
     event.preventDefault();
   };
 
+  onRemoveMessage = uid => {
+    this.props.firebase.message(uid).remove();
+  };
+
   render() {
     const { text, messages, loading } =  this.state;
 
@@ -74,7 +78,7 @@ class MessagesBase extends Component {
             {loading && <div>Loading ...</div>}
 
             {messages ? (
-              <MessageList messages={messages} />
+              <MessageList messages={messages} onRemoveMessage={this.onRemoveMessage} />
             ) : (
               <div>There are no messages ...</div>
             )}
@@ -90,17 +94,20 @@ class MessagesBase extends Component {
   }
 }
 
-const MessageList = ({ messages }) => (
+const MessageList = ({ messages, onRemoveMessage }) => (
   <ul>
     {messages.map(message => (
-      <MessageItem key={message.uid} message={message} />
+      <MessageItem key={message.uid} message={message} onRemoveMessage={onRemoveMessage} />
     ))}
   </ul>
 );
 
-const MessageItem = ({ message }) => (
+const MessageItem = ({ message, onRemoveMessage }) => (
   <li>
     <strong>{message.userId}</strong> {message.text}
+    <button type="button" onClick={() => onRemoveMessage(message.uid)}>
+      Delete
+    </button>
   </li>
 );
 
